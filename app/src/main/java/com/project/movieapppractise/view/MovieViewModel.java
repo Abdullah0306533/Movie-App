a70b52071e097a34e7bd19a9528732e1072a2a00
Abdullah0306533 <163910555+Abdullah0306533@users.noreply.github.com>
2024-05-12 09:33:55 +0500

    View Model Integrated

---
+++ app/src/main/java/com/project/movieapppractise/view/MovieViewModel.java
@@ -0,0 +1,38 @@
+package com.project.movieapppractise.view;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.project.movieapppractise.model.Movie;
+import com.project.movieapppractise.model.MovieRepository;
+
+import java.util.List;
+
+/**
+ * ViewModel for managing movie data and providing it to the UI.
+ */
+public class MovieViewModel extends AndroidViewModel {
+    private final MovieRepository repository;
+
+    /**
+     * Constructor for the MovieViewModel.
+     *
+     * @param application The application context.
+     */
+    public MovieViewModel(@NonNull Application application) {
+        super(application);
+        repository = new MovieRepository(application);
+    }
+
+    /**
+     * Gets a LiveData object containing a list of all movies.
+     *
+     * @return LiveData object containing a list of movies.
+     */
+    public LiveData<List<Movie>> getAllMovies() {
+        return repository.getMutableLiveData();
+    }
+}
